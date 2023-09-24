955fd69ca5fc6b47f9c1740a066b19ab973e8d42
espnal <78657757+espnal@users.noreply.github.com>
2023-09-24 00:24:17 -0400

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,5 @@
 const mongodb = require('../db/connection');
-const { ObjectId } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
     const result = await mongodb.getDb().db().collection('contacts').find()
